78655fde6f2cd4dfa728c2c49a81eb57b246f2ca
jeffersonamado <jeffersonamado@gmail.com>
2023-09-02 11:31:15 -0500

    feat: Add login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,41 @@ import {
 import Home from './components/Home';
 import Products from './components/Products';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function App() {
   return (
     <Router>
-      <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/home">Tienda CI/CD</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Principal</Nav.Link>
+            <Nav.Link href="/products">Productos</Nav.Link>
+            <Nav.Link href="/orders">Ordenes</Nav.Link>
+            <NavDropdown title="Operaciones" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Crear producto</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Crear orden</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Despachos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Clientes</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      {/* <div>
         <ul>
           <li><Link to="/home">Principal</Link></li>
           <li><Link to="/products" >Productos</Link></li>
         </ul>
       </div>
-      <hr />
+      <hr /> */}
         <Routes>
           <Route exact path="/home" element={<Home/>} />
           <Route path="/login" element={<Home/>} />
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,47 @@
 import * as React from 'react'
+import { useState } from 'react';
+
+import { Card } from 'react-bootstrap';
 
 const Home =()=>{
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const sendLogin = ()=>{
+    if(!!email || !!password) return;
+    fetch("http://localhost:9030/login", {
+      method:'POST'
+    })
+  }
+
   return (
-    <p>
-      Pagina Principal!
-    </p>
+    <Card style={{ margin:'30px' }} className="justify-content-center align-items-center">
+      <Card.Body >
+          <div style={{ width:'100%', textAlign:'center' }} >
+            <form class="form" method="POST">
+            <h1>Tienda CI/CD</h1>
+            <span>
+              Somos una Tienda de prueba para CI/CD
+            </span>
+              <div class="form-group">
+                  <label for="email" class="text-info"></label>
+                  <input type="email" name="email" id="email" class="form-control" 
+                      onChange={(e)=> setEmail(e.target.value)} defaultValue={email} />
+              </div>
+              <div class="form-group">
+                  <label for="password" class="text-info"></label>
+                  <input type="password" name="password" id="password" class="form-control" 
+                      onChange={(e)=> setPassword(e.target.value)} defaultValue={password} />
+              </div>
+              <hr/>
+              <div className='row'>
+                <button className='btn btn-sm btn-primary col-9 mx-3' type='submit'>Acceder</button>
+                <button className='btn btn-sm btn-secondary col-1' type='submit'>?</button>
+              </div>
+          </form>
+        </div>
+        </Card.Body>
+    </Card>
   )
 };
 
